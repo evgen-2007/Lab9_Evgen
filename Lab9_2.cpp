71d70e1473c8e4bbd1a4267d8a5b5a11bd4a0d11
evgen-2007 <mamontovevgen2007@gmail.com>
2024-10-28 18:40:42 +0200

    Add files via upload

---
+++ Lab9_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+
+
+	float a, b, A, B, C, D;
+	printf("vv chislo odin\n");
+	scanf_s("%f", &a);
+	printf("vv chislo dva\n");
+	scanf_s("%f", &b);
+	A = a + b;
+	B = a - b;
+	C = a * b;
+	D = a / b;
+	printf("Suma = %f\n", A);
+	printf("Riznica = %f\n", B);
+	printf("Mnozhena = %f\n", C);
+	printf("Dilenna = %f\n", D);
+
+
+	return 0;
+}
